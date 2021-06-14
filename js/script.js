4c95fb6cfd8662b09b9ad51715a3ba7e6f638b35
AlexTaglia <alex.tagliabue@gmail.com>
2021-06-14 14:22:42 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/*
+Un alert() espone 5 numeri generati casualmente.
+Da lì parte un timer di 30 secondi.
+Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, 
+tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da 
+indovinare sono stati individuati.
+*/
+
+// Creare funzione numeri random 
+
+// visualizzare in un alert() i 5 numeri che magari metto in un array
+
+// Far partire un timer di 30 secondi
+
+// Dopo il timer, faccio inserire i 5 numeri in dei prompt e pusho i numeri che ha inserito l'utente in un array 
+
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da 
+// indovinare sono stati individuati potrei usare un includes()
+
+
